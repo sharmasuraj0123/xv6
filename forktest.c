94ce26400c0c372aa338ae06a8dc75e641aa2427
Student Spring 2017 <student@vl78.CS.STONYBROOK.EDU>
2017-10-19 22:05:22 -0400

    added a second test

--- forktest.c
+++ forktest.c
@@ -1,9 +1,15 @@
 // Test that fork fails gracefully.
 // Tiny executable so that the limit can be filling the proc table.
 
+#include "param.h"
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h" 
 
 #define N  1000
 
@@ -48,7 +54,8 @@ forktest(void)
   printf(1, "fork test OK\n");
 }
 
-
+//check to see if the child process can edit a parent variable.
+//if not then cow-fork works.
 void
 forktest2(void)
 {
@@ -61,12 +68,38 @@ forktest2(void)
   }
   wait();
   if (pid == 0) {
-    if(i == 0){
-      printf(1,"parent copy did not change");
-    }
-    else{
-      printf(1,"parent copy changed");
-    }
+    if(i == 0)
+      printf(1,"parent copy did not change\n");
+    else
+      printf(1,"parent copy changed\n");
+  }
+}
+
+
+//check to see if the child process has different fd from the parent
+void
+forktest3(void)
+{
+  int fd;
+  char buf[10]={'\0'};
+  int pid;
+  int n=0;
+  fd = open("wolfietest.c",O_RDONLY);
+
+  if (fd < 0)
+    printf(1, "fd not valid");
+  pid = fork();
+  if(pid > 0)
+    close(fd);
+
+  
+  if(pid == 0){
+    wait();
+    n=read(fd, buf, sizeof(buf));
+    if(n<0)
+      printf(1,"couldn't read a closed file, the child closed correctly\n");
+    else
+      printf(1,"somehow read from a closed file\n");
   }
 }
 
@@ -74,6 +107,7 @@ forktest2(void)
 int
 main(void)
 {
+  forktest3();
   forktest2();
   forktest();
   exit();
